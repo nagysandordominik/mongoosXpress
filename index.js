6404e328c66d697c3c697abbcb8cc0188d0aa79b
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-04-14 08:22:08 +0200

    add: linked with mongoose

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/test')
+    .then(() => {   
+        console.log('Connection is open! - Mongo')
+    })
+    .catch(err => {
+        console.log('Error occured - Mongo')
+        console.log(err)
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
