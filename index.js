181a2da8a8cc27316a530049d7270b20052bb9d4
Nagy Sándor Dominik <thesatyka@gmail.com>
2022-04-18 14:09:39 +0200

    fix: app.put res.send property deleted

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
-const method = require('method-override')
+const methodOverride = require('method-override')
 
 
 const Product = require('./models/product');
@@ -20,6 +20,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
+app.use((err, req, res, next) => {
+    const {status = 500, message = 'Something went wrong'} = err;
+    res.status(status).send(message)
+})
 
 app.get('/products', async (req,res) => {
     const products = await Product.find({})
@@ -44,17 +48,17 @@ app.get('/products/:id', async (req,res) =>  {
     res.render('products/show', {product})
 }) 
 
+
 app.get('/products/:id/edit', async (req,res) =>  {
     const {id} = req.params;
     const product = await Product.findById(id);
-    res.render('products/show', {product})
+    res.render('products/edit', {product})
 }) 
 app.put('/products/:id', async (req,res) => {
     const {id} = req.params;
     const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
     res.redirect(`/products/${product._id}`)
     console.log(req.body);
-    res.send('Put!')
 })
 
 app.listen(3000, () => {
